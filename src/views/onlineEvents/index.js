2108f5f840979162af1c6178f332e89d4b5522dd
Ananthakr <ananthakumartpmstar@gmail.com>
2018-01-08 07:08:47 +0000

    fixed icons

--- src/views/onlineEvents/index.js
+++ src/views/onlineEvents/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import './onlineevents.css';
 import Menu from '../../components/menu';
 import GenericBox from  '../../components/genericBox';
-import GenImage from '../../images/icons/circus.png';
+import Cloud from '../../images/icons/cloud.png';
 import data from './online.json';
 
 
@@ -16,7 +16,7 @@ export default class OnlineEvents extends Component{
     displayEvents(){
         var rows = data.map((res,k) => {return(
             <div key={k} className="col-md-4">
-                <Link to={{pathname: "/events/"+res.title,state: { info: res.content} }}><GenericBox title={res.title} image={GenImage} imageTitle="TechIcon"/></Link>
+                <Link to={{pathname: "/events/"+res.title,state: { info: res.content} }}><GenericBox title={res.title} image={Cloud} imageTitle="TechIcon"/></Link>
             </div>
         )}).reduce((r,e,i) => {
             i%3===0&&r.push([]);
