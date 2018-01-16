5fc93fd276076f6ffec0100a7b9fbdb4c8148d62
Ananthakr <ananthakumartpmstar@gmail.com>
2018-01-16 22:01:08 +0000

    fixed workshops reveal date

--- src/views/workshops/index.js
+++ src/views/workshops/index.js
@@ -15,7 +15,7 @@ export default class Workshops extends Component{
                 </div>
                 <div className="container-fluid events-container">
                     <div className="row">
-                        <p className="info-text">&lt;!--Revealing on Jan 12 --&#47;&gt;</p>
+                        <p className="info-text">&lt;!--Revealing on Jan 20 --&#47;&gt;</p>
                     </div>
                 </div>
             </main>
